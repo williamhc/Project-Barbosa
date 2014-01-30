7fc32e5dbfb8ee9140d84891e21f4e52e35f55a8
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-01-30 14:16:42 -0600

    add credentials for fb, twitter, google login

--- config/secrets.js
+++ config/secrets.js
@@ -2,22 +2,22 @@ module.exports = {
   db: 'localhost',
 
   facebook: {
-    clientID: 'Your App ID',
-    clientSecret: 'Your App Secret',
+    clientID: '1453279814895954',
+    clientSecret: 'a09037ab9a66a012e428b7ccfdadd7c7',
     callbackURL: '/auth/facebook/callback',
     passReqToCallback: true
   },
 
   twitter: {
-    consumerKey: 'Your Consumer Key',
-    consumerSecret: 'Your Consumer Secret',
+    consumerKey: 'TxSKULtrqa6XR43I6YEeqQ',
+    consumerSecret: 'ZrZoRV2hooWeKj2rHoUt0nm2Xe7rPhHYUyLoQp1x0',
     callbackURL: '/auth/twitter/callback',
     passReqToCallback: true
   },
 
   google: {
-    clientID: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
+    clientID: '58067519186-5qma5qcpp0snm1285ovbmhdqh3qahq95.apps.googleusercontent.com',
+    clientSecret: 'TXJK9CQsNexI5QENCC37uDN-',
     callbackURL: '/auth/google/callback',
     passReqToCallback: true
   }
