23c4e67c7baeadae9b645c417403dbc614d70028
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-02-28 10:45:30 -0600

    update trip controller to contain what we need for trips

--- controllers/trip.js
+++ controllers/trip.js
@@ -1,17 +1,48 @@
 var mongoose = require('mongoose');
 var Trip = require('../models/Trip');
+var extend = require('util')._extend;
 
 /**
- * Load
+ * PUT /trips/:id
+ * Update a trip.
  */
 
-exports.load = function(req, res, next, id){
-  Trip.load(id, function (err, trip) {
-    if (err) return next(err)
-    if (!trip) return next(new Error('not found'))
-    req.trip = trip
-    next()
-  })
+exports.updateTrip = function(req, res){
+  var Trip = mongoose.model('Trip');
+  Trip.findById(req.params.tripid).exec(function(err, trip) {
+    if (err) {
+      res.status(500).json(null);
+    } else {
+      trip = extend(trip, req.body.trip)
+      trip.save(function (err) {
+        if (err) {
+          res.status(500).json(null);
+        };
+        res.json({trip:trip});
+      })
+    }
+  });
+}
+
+/**
+ * DELETE /trips/:id
+ * Delete a trip.
+ */
+
+exports.deleteTrip = function(req, res){
+  var Trip = mongoose.model('Trip');
+  Trip.findById(req.params.tripid).exec(function(err, trip) {
+    if (err) {
+      res.status(500).json(null);
+    } else {
+      trip.remove(function (err) {
+        if (err) {
+          res.status(500).json(null);
+        };
+        res.json({});
+      })
+    }
+  });
 }
 
 /**
@@ -25,7 +56,23 @@ exports.showTrip = function(req, res) {
             if (err) {
                 res.status(500).json(null);
             } else {
-                res.json(trips);
+                res.json({trip:trips});
+            }
+        });
+};
+
+/**
+ * GET /trips
+ * Get all trips
+ */
+
+exports.listTrips = function(req, res) {
+  var Trip = mongoose.model('Trip');
+  Trip.find().exec(function(err, trips) {
+            if (err) {
+                res.status(500).json(null);
+            } else {
+                res.json({'trips':trips});
             }
         });
 };
@@ -47,11 +94,9 @@ exports.renderCreate = function(req, res) {
  */
 
 exports.createTrip = function(req, res) {
-  trip = new Trip({
-    name: req.body.tripname
-  })
+  trip = new Trip(req.body.trip)
   trip.save(function(err, trip){
     if(err) return console.error(err);
   });
-  res.json(trip)
+  res.json({trip:trip})
 };
