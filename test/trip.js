f50df11ad083d2239549e69095ec89b9061ebae7
umcook73 <umcook73@gmail.com>
2014-03-01 16:47:47 -0600

    New Test
    Re did the test for checking for a bad id, and fixed the formatting that
    was wrong with the last one.

--- test/trip.js
+++ test/trip.js
@@ -4,18 +4,22 @@ var express = require('express');
 var app = require('../app.js');
 var request = require('supertest');
 
-describe('GET /trips/:badid', function(){
-	it('should return json with a 500 error');
+describe('GET /trips/:badid', function() {
+  it('should return json with a 500 error', function(done) {
+    request(app)
+      .get('/trips/:badid')
+      .expect('Content-Type', /json/)
+      .expect(500, done);
+  })
 })
 
 describe('POST /trips', function(){
-	
-	it('should respond with json', function(done){
-		request(app)
-			.post('/trips')
-			.send({trip:{name: "TEST_TRIP"}})
-			.set('Accept','application/json')
-			.expect('Content-Type', /json/)
-			.expect(200, done);
+  it('should respond with json', function(done){
+    request(app)
+      .post('/trips')
+      .send({trip:{name: "TEST_TRIP"}})
+      .set('Accept','application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
 	})
 });
