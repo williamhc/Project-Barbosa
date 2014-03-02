d02b75003217d9d24de64d77cbd08b7b59dcffc7
yuriydyrenko <mail@yuriy.me>
2014-03-02 15:14:30 -0600

    Converted tabs to 2 spaces

--- test/app.js
+++ test/app.js
@@ -5,19 +5,19 @@ var app = require('../app.js');
 var request = require('supertest');
 
 describe('GET /', function() {
-	it('should be return html', function(done) {
-		request(app)
-			.get('/')
-			.expect('Content-Type', /html/)
-			.expect(200, done);
-	})
+  it('should be return html', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200, done);
+  })
 })
 
 describe('GET /NOTHINGHERE', function() {
-	it('should return 404', function(done) {
-		request(app)
-			.get('/NOTHINGHERE')
-			.expect('Content-Type', /html/)
-			.expect(404, done);
-	})
+  it('should return 404', function(done) {
+    request(app)
+      .get('/NOTHINGHERE')
+      .expect('Content-Type', /html/)
+      .expect(404, done);
+  })
 })
